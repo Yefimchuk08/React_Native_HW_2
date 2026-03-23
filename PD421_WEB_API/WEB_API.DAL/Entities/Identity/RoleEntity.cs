eb140157296d8ce8112c66bd940930327cac095a
novakvova <novakvova@gmail.com>
2026-03-23 15:36:04 +0200

    Add Identity

---
+++ PD421_WEB_API/WEB_API.DAL/Entities/Identity/RoleEntity.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WEB_API.DAL.Entities.Identity;
+
+public class RoleEntity : IdentityRole<long>
+{
+    public virtual ICollection<UserRoleEntity>? UserRoles { get; set; } = new List<UserRoleEntity>();
+    public RoleEntity() : base() { }
+
+    public RoleEntity(string roleName) : base(roleName) { }
+}
---
+++ PD421_WEB_API/WEB_API.DAL/Entities/Identity/UserRoleEntity.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WEB_API.DAL.Entities.Identity;
+
+public class UserRoleEntity : IdentityUserRole<long>
+{
+    public virtual UserEntity? User { get; set; }
+    public virtual RoleEntity? Role { get; set; }
+}
